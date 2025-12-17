80b1bfc870f47e1fb624cb1d4abdfb7369313e88
Vijaykrishnan10 <krishvijay1000@gmail.com>
2025-12-17 12:40:05 +0530

    Create properties.js

---
+++ properties.js
@@ -0,0 +1,17 @@
+const properties = [
+  {
+    type: "Land",
+    title: "DTCP Approved Land – Thekkalur",
+    location: "Thekkalur",
+    water: "Available",
+    eb: "Available",
+    approval: "DTCP Approved",
+    price: "₹30 Lakhs",
+    images: [
+      "images/land1.jpg",
+      "images/land2.jpg",
+      "images/land3.jpg"
+    ],
+    map: "https://maps.google.com/?q=Thekkalur"
+  }
+];
